9075b95f9057d812cb0c92a73bf35907fe93eabc
Ronan Guilloux <ronan.guilloux@gmail.com>
2015-01-21 17:50:42 +0100

    Command line to generate fake documents

---
+++ src/LesPolypodes/SimpleDMSBundle/Command/FakeDocumentsCommand.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace LesPolypodes\SimpleDMSBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Faker;
+use PhpOffice;
+
+class FakeDocumentsCommand extends ContainerAwareCommand
+{
+
+    protected $availableFormats = ['docx', 'txt', 'md', 'jpg'];
+    protected $randomFonts = ["Arial", "Comic Sans MS", "Courier New", "Georgia", "Times New Roman", "Trebuchet MS", "Verdana"];
+    protected $maxQuantity = 50;
+    protected $resources = null;
+    protected $faker = null;
+
+    protected function configure()
+    {
+        $this
+            ->setName('dms:fake')
+            ->setDescription('Generate dozens of fake documents')
+            ->addArgument('extension', InputArgument::OPTIONAL, 'Specify extension to generate: docx, jpg, txt, pdf, md or all')
+            ->addArgument('quantity', InputArgument::OPTIONAL, 'Specify quantity to generate: from 1 (default) to '.$this->maxQuantity)
+        ;
+
+        //$this->faker = Faker::cre
+    }
+
+    protected function createDocument($extension = 'docx', $quantity = 1)
+    {
+        $fakeDir = $this->getContainer()->get('file_locator')->locate('@LesPolypodesSimpleDMSBundle/Resources/fake');
+        $result = null;
+        if ($quantity > $this->maxQuantity) {
+            throw new \InvalidArgumentException("Veuillez ne pas créer plus de 50 documents à la fois.");
+        }
+        switch ($extension) {
+            case "jpg":
+                for ($i = 0; $i < $quantity; $i++) {
+                    $result[] = $this->createImage($fakeDir);
+                }
+                break;
+            case "txt":
+                for ($i = 0; $i < $quantity; $i++) {
+                    $result[] = $this->createTxtFile($fakeDir);
+                }
+                break;
+            case "md": // markdown
+                for ($i = 0; $i < $quantity; $i++) {
+                    $result[] = $this->createMarkdownFile($fakeDir);
+                }
+                break;
+            case "pdf": // markdown
+                for ($i = 0; $i < $quantity; $i++) {
+                    $result[] = $this->createPdfFile($fakeDir);
+                }
+                break;
+            case "docx":
+                for ($i = 0; $i < $quantity; $i++) {
+                    $result[] = $this->createWordFile($fakeDir);
+                }
+                break;
+            case "all":
+                for ($i = 0; $i < $quantity; $i++) {
+                    $result[] = $this->createWordFile($fakeDir);
+                    $result[] = $this->createTxtFile($fakeDir);
+                    $result[] = $this->createMarkdownFile($fakeDir);
+                    $result[] = $this->createPdfFile($fakeDir);
+                    $result[] = $this->createImage($fakeDir);
+                }
+                break;
+            default:
+                throw new \InvalidArgumentException(sprintf("%s : %s", "Please give me a valid extension", implode(", ", $this->availableFormats)));
+                break;
+        }
+
+        return $result;
+    }
+
+    protected function createImage($fakeDir)
+    {
+        $image = Faker\Provider\Image::image();
+        $result = sprintf("%s/%s", $fakeDir, basename($image));
+        rename($image, $result);
+
+        return $result;
+    }
+
+    protected function createTxtFile($fakeDir)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        $elements = array(
+           "\t".$faker->sentence(),
+        );
+
+        for ($i = 0; $i <= mt_rand(10, 30); $i++) {
+            $elements[] = $faker->paragraph(mt_rand(30, 50));
+        }
+
+        $elements[] = " fake";
+        $txt = implode("\n\r\n\r", $elements);
+
+        $result = sprintf("%s/%s.%s", $fakeDir, $faker->slug, 'txt');
+        file_put_contents($result, $txt);
+
+        return $result;
+    }
+
+    protected function createMarkdownFile($fakeDir)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        $elements = array(
+            "#".$faker->sentence(),
+            "Edited by: [".$faker->company."](".$faker->url.").",
+        );
+
+        for ($i = 0; $i <= mt_rand(10, 30); $i++) {
+            $elements[] = "##".$faker->sentence();
+            $elements[] = "__".$faker->sentence()."__ ".$faker->paragraph(mt_rand(30, 50));
+        }
+        $elements[] = " fake";
+        $txt = implode("\n\r\n\r", $elements);
+
+        $result = sprintf("%s/%s.%s", $fakeDir, $faker->slug, 'md');
+        file_put_contents($result, $txt);
+
+        return $result;
+    }
+
+    protected function createPdfFile($fakeDir)
+    {
+        $pandocLocation = exec('which pandoc');
+        if (false == strpos($pandocLocation, 'pandoc')) {
+            throw new \InvalidArgumentException("le format PDF ne peut être généré: installer Pandoc: http://johnmacfarlane.net/pandoc/installing.html");
+        }
+
+        $markdown = $this->createMarkdownFile($fakeDir);
+        $result = str_replace('.md', '.pdf', $markdown);
+        exec(sprintf("pandoc %s -o %s", $markdown, $result));
+        unlink($markdown);
+
+        return $result;
+    }
+
+    protected function createWordFile($fakeDir)
+    {
+        $randomFont = $this->randomFonts[array_rand($this->randomFonts)];
+        $faker = Faker\Factory::create('fr_FR');
+
+        $phpWord = new PhpOffice\PhpWord\PhpWord();
+        $phpWord->addTitleStyle(1, array('name' => $randomFont, 'size' => 26));
+        $phpWord->addTitleStyle(2, array('name' => $randomFont, 'size' => 16));
+        $section = $phpWord->addSection();
+        $section->addTitle($faker->sentence(), 1);
+        $section->addTextBreak();
+        for ($i = 0; $i <= mt_rand(10, 30); $i++) {
+            $section->addTitle($faker->sentence(), 2);
+            $section->addTextBreak();
+            $section->addText(
+                htmlspecialchars($faker->paragraph(mt_rand(30, 50))),
+                array(
+                    'name'  => $randomFont,
+                    'size'  => 11,
+                    //'color' => str_replace('#', '', $faker->hexcolor)
+                )
+            );
+
+            $section->addText(" fake");
+            $section->addTextBreak();
+        }
+        $objWriter = PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+        $result = sprintf("%s/%s.%s", $fakeDir, $faker->slug, 'docx');
+        $objWriter->save($result);
+
+        return $result;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $extension = $input->getArgument('extension');
+        $quantity = $input->getArgument('quantity');
+        $extension = (!empty($extension)) ? $extension : 'docx';
+        $quantity = (!empty($quantity)) ? $quantity : 1;
+        $files = $this->createDocument($extension, $quantity);
+        $output->writeln(sprintf("%d document(s) created in %s", $quantity, dirname($files[0])));
+    }
+}
