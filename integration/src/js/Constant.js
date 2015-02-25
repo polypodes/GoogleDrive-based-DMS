9a935281858cf80247d709b30797d87d60cb4743
Ronan Guilloux <ronan.guilloux@gmail.com>
2015-02-25 15:33:10 +0100

    fixing API hostname & routes

--- integration/src/js/Constant.js
+++ integration/src/js/Constant.js
@@ -1,11 +1,12 @@
-var baseUrl = 'http://localhost/app_dev.php/';
+var baseUrl = '';
 
 module.exports = {
-    'API_URL': baseUrl + 'api/',
-    'API_GET_FOLDERS': baseUrl + 'api/folders',
-    'API_GET_FILES': baseUrl + 'api/files',
-    'API_GET_LAST_FILES': baseUrl + 'api/lastmodified',
-    // 'API_GET_LAST_FILES': baseUrl + 'api/files',
-    'API_GET_FILES_TYPE': baseUrl + 'api/filetypes',
-    'API_GET_FILES_BY_TYPE': baseUrl + 'api/files/type'
+    'API_URL': baseUrl + '/api/',
+    'API_GET_FILE': baseUrl + '/files/',
+    'API_GET_FOLDERS': baseUrl + '/api/folders',
+    'API_GET_FILES': baseUrl + '/api/files',
+    'API_GET_LAST_FILES': baseUrl + '/api/lastmodified',
+    // 'API_GET_LAST_FILES': baseUrl + '/api/files',
+    'API_GET_FILES_TYPE': baseUrl + '/api/filetypes',
+    'API_GET_FILES_BY_TYPE': baseUrl + '/api/files/type'
 }
