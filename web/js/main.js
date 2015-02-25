e73ba335944a71816b04a239f06b8b25779434c3
Ronan Guilloux <ronan.guilloux@gmail.com>
2015-02-25 12:09:14 +0100

    cleaning

--- web/js/main.js
+++
@@ -1,4 +0,0 @@
-$( document ).ready(function() {
-    ZeroClipboard.config( { swfPath: "/bower_components/zeroclipboard/dist/ZeroClipboard.swf" } );
-    var client = new ZeroClipboard($(".copy-button"));
-});
